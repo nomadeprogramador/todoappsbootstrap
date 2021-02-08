3f6c6e7e1514316a9754d56633c212840fd0efa1
jiron <nomadeprogramador@gmai.com>
2021-02-08 00:46:54 -0300

    to do app

--- tarefa/views.py
+++ tarefa/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Tarefa
 # Create your views here.
+def index (request):
+    tarefa=Tarefa.objects.all()
+    if request.POST:
+        tarefa=request.POST.get('tarefa')
+        tarefa=Tarefa(conteudo=tarefa)
+        tarefa.save()
+        return redirect('/')
+    context={
+        'tarefa':tarefa,
+    }
+    return render(request,'tarefas/index.html',context)
+def excluir_tarefa(request,id):
+    tarefa=Tarefa.objects.get(id=id)
+    tarefa.delete()
+    return redirect('/')
